5b3a5c691cfa09f9f5d3052eb2b39a11ba7026e2
PrzygoKNR <jakubprzygodzki@gmail.com>
2018-04-04 00:07:26 +0200

    poprawione wczytywanie i dodane komentarze

--- Ant/AntMovement.c
+++ Ant/AntMovement.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "AntMovement.h"
-int makeHeading(int head , char handling)
+int makeHeading(int head , char handling) // funkcja zmieniająca kierunek poruszania się mrówek
 {
-    switch(handling)
+    switch(handling) 
     {
         case 'L': head = (head + 1);
             break;
@@ -20,20 +20,20 @@ int makeHeading(int head , char handling)
     return head;
 }
 
-int checkActive(int* activityTab, int antCounter)
+int checkActive(int* activityTab, int antCounter) // funkcja sprawdzająca czy zosatała przynajmniej jedna aktywna mrówka
 {
     int counter = 0;
     for(int i=0; i<antCounter; i++)
     {
-        if(activityTab[i] == 1 ){ counter++; }
+        if(activityTab[i] == 1 ){ counter++; } // jeżeli mrówka jest aktywna to zwiększ o jeden 
     }
 
-    if(counter>0){return 1;}
+    if(counter>0){return 1;} //jeżeli przynajmniej jeden to zwracamy aktywność
 
     return 0;
 }
 
-int acivityCheck(int x, int y, int boardSize)
+int acivityCheck(int x, int y, int boardSize) // funkcja sprawdza czy mrówka nie wyszła poza planszę 
 {
     if(x < 0 || x >boardSize){return 0;}
     if(y < 0 || y >boardSize){return 0;}
@@ -41,58 +41,58 @@ int acivityCheck(int x, int y, int boardSize)
     return 1;
 }
 
-int makeFootprint(int fieldInfo , int handlingLenght)
+int makeFootprint(int fieldInfo , int handlingLenght) //funkcja dodająca ślad na polu
 {
     fieldInfo++;
-    if( fieldInfo == handlingLenght ){ fieldInfo == 0; }
+    if( fieldInfo == handlingLenght ){ fieldInfo == 0; } // jeżeli osiągnięto długość instrukcji sterowania to zerujemy
     return fieldInfo;
 }
 
-void moveAnt ( int* x, int* y, int* heading, int antNumber, int fieldInfo,int** board, int handlingLenght, char handling )
+void moveAnt ( int* x, int* y, int* heading, int antNumber, int fieldInfo,int** board, int handlingLenght, char handling )  // funkcja poruszająca mrówką
 {
 
-    board[ x[antNumber] ][ y[antNumber] ] = makeFootprint( fieldInfo, handlingLenght);
+    board[ x[antNumber] ][ y[antNumber] ] = makeFootprint( fieldInfo, handlingLenght);  // dodajemy ślad
 
-    switch (*heading)
+    switch (*heading)									// kierunek:
     {
-        case 0:
+        case 0:                                       	//Północ
             if(handling == 'L'){x[antNumber] -= 1;}
             else if(handling =='R'){x[antNumber] += 1;}
             break;
-        case 1:
+        case 1:											//Zachód 
             if(handling == 'L'){y[antNumber] -= 1;}
             else if(handling =='R'){y[antNumber] += 1;}
             break;
-        case 2:
+        case 2:											//Południe
             if(handling == 'L'){x[antNumber] += 1;}
             else if(handling =='R'){x[antNumber] -= 1;}
             break;
-        case 3:
+        case 3:											//Wschód
             if(handling == 'L'){y[antNumber] += 1;}
             else if(handling =='R'){y[antNumber] -= 1;}
             break;
     }
 
-    heading[antNumber] = makeHeading( *heading, handling);
+    heading[antNumber] = makeHeading( *heading, handling); // zmieniamy kierunek poruszania
 }
-void antAlgorithm ( int* x, int* y, int antCounter, int* heading, int** board, int boardSize, int handlingLenght, char* handling)
+void antAlgorithm ( int* x, int* y, int antCounter, int* heading, int** board, int boardSize, int handlingLenght, char* handling) // funkcja odpowiadająca za cały algorytm mrówki
 {
-    int* activityTab = (int *)malloc(antCounter* sizeof(int));
+    int* activityTab = (int *)malloc(antCounter* sizeof(int)); // tablica odpowiadająca za aktywność mrówek (jeżeli mrówka wyjdzie za granicę planszy to ją wyłącza)
     for(int j=0;j<antCounter;j++)
     {
-        activityTab[j] = 1;
+        activityTab[j] = 1;        // wszystkie mrówki aktywne na starcie
     }
-    int active = 1;
-    int temporaryFieldInfo;
+    int active = 1; 			// zmienna odpowiadająca za wyłączania pętli przechodzenia gdy wszystkie mrówki będą miały status: nieaktywna(0) to wyłącza ją 
+    int temporaryFieldInfo;    
     while(active) {
-        for (int i = 0; i < antCounter; i++) {
-            if (activityTab[i] == 1) {
-                temporaryFieldInfo =board[x[i]][y[i]];
-                moveAnt(x, y, heading, i, board[x[i]][y[i]], board, handlingLenght, handling[temporaryFieldInfo]);
-                activityTab[i] = acivityCheck( x[i], y[i], boardSize);
+        for (int i = 0; i < antCounter; i++) { // iteracja po każdej z mrówek
+            if (activityTab[i] == 1) {  // sprawdzamy czy mrówka jest aktywna
+                temporaryFieldInfo =board[x[i]][y[i]];  // wczytujemy informację z pola
+                moveAnt(x, y, heading, i, board[x[i]][y[i]], board, handlingLenght, handling[temporaryFieldInfo]); //przekazujemy do funkcji poruszającej
+                activityTab[i] = acivityCheck( x[i], y[i], boardSize); //sprawdzamy czy mrówka nie wyszła poza planszę
                 }
             }
         }
-        active = checkActive( activityTab, antCounter);
+        active = checkActive( activityTab, antCounter); // sprawdzamy czy zostały jakieś aktywne mrówki 
     }
 }
--- Ant/initialization.c
+++ Ant/initialization.c
@@ -5,28 +5,47 @@
 #include <stdio.h>
 #include "initialization.h"
 
+int checkInit(int* x, int* y, int* antCounter, int* heading, int boardSize, int* handlingLenght, char* handling)
+{
+	if(boardSize < 9 || boardSize > 641) { return 0; printf("zly rozmiar planszy");}
+	if(antCounter < 1 || antCounter > 5) { return 0; printf("zly rozmiar planszy");}
+	for(int i = 0; i < antCounter; i++){
+		if( x[i] < 0 || x[i] > boardSize) { return 0; printf("zly parametr początkowy x mrowki %d",i);}
+		if( y[i] < 0 || y[i] > boardSize) { return 0; printf("zly parametr początkowy y mrowki %d",i);}
+		if( heading[i] < 0 || heading[i] > 3) { return 0; printf("zly parametr początkowy kierunku mrowki %d",i);}
+	}
+	for(int i = 0; i < handlingLenght; i++){
+		if(handling[i] != L && handling != R){return 0; printf("błąd %d elementu sterowania", i);
+	}
+	return boardSize;
+}
 
-int readFromFile(int* x, int* y, int* antCounter, int* heading, int boardSize, int* handlingLenght, char* handling)
+int readFromFile(int* x, int* y, int* antCounter, int* heading, int boardSize, int* handlingLenght, char* handling) // funkcja czytająca z pliku 
 {
 	int buf = 0;
 	FILE * file;
 	file = fopen( "initialization.txt" , "r");
-	fscanf(file, %d, &boardSize);
-	fscanf(file, %d, antCounter);
-	x = malloc(sizeof(int) * (*antCounter));
+	fscanf(file, %d, &boardSize); // wczytaj rozmiar planszy 
+	fscanf(file, %d, antCounter); // wczytaj ilość mrówek
+	x = malloc(sizeof(int) * (*antCounter)); // allokacja poszczególnych tablic x y i kierunek
 	y = malloc(sizeof(int) * (*antCounter));
 	heading = malloc(sizeof(int) * (*antCounter));
-	for(int i = 0;i < (*antCounter); i++){
+	
+	for(int i = 0;i < (*antCounter); i++){   // iterujemy się po wszytskich mrówkach i pokolei wczytujemy parametry startowe
 	fscanf(file, %d, &x[i]);
 	fscanf(file, %d, &y[i]);
 	fscanf(file, %d, &heading[i]);
 	}
-	fscanf(file, %d, handlingLenght);
+	
+	fscanf(file, %d, handlingLenght);  // wczytujemy długość instrukcji sterującej
 	handling = malloc(sizeof(char) * (*handlingLenght));
-	for(int i=0; i < (*handlingLenght); i++){
-	fscanf(file, %c, &handling[i]);
+	
+	for(int i=0; i < (*handlingLenght); i++){   // wczytujemy pokolei sterowanie
+		*handling++ = (char)fgetc(file);
 	}
 	
+	boardSize = checkInit;
 	fclose(file);
+	
 	return boardSize;
 }
