2a4a6043c6da66487d0b61265f40e46785d0fcc7
AdamSuski <adamus.suski@gmail.com>
2018-05-10 19:37:28 +0200

    porzadek

--- Nowy folder/Ant/initialization.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INITIALIZATION_H
-#define _INITIALIZATION_H
-
-int checkInit( int*, int*, int*, int*, int, int*, char* );
-
-int readFromFile(int* x, int* y, int* antCounter, int* heading, int boardSize, int* handlingLenght, char* handling, char* fileARG);
-
-#endif //INITIALIZATION_H
--- Nowy folder/alokacjaTasmy/board.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BOARD_H
-#define _BOARD_H
-//generating 2dim board where status of ant is saved
-int** boardGenerate( int );
-//displaying board
-void boardDisplay(int**, int);
-
-#endif
