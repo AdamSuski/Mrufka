fc21adf587c862782445d94339771884e7e17e88
PrzygoKNR <jakubprzygodzki@gmail.com>
2018-05-27 22:42:27 +0200

    jakieś początki

---
+++ projektJAVA/src/langtonsant/Ant.java
@@ -0,0 +1,18 @@
+package langtonsant;
+
+import java.awt.Point;
+
+public class Ant {
+
+    public Ant(Point startingPosition)
+    {
+        this.position = startingPosition;
+    }
+
+    Point position;
+
+    public void makeStep()
+    {
+
+    }
+}
