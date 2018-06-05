e4ccb3a7886c74c3c3ce06a93f1fa7bdd578c111
PrzygoKNR <jakubprzygodzki@gmail.com>
2018-06-05 20:02:55 +0200

    XD

--- projektJAVA/src/langtonsant/Ant.java
+++ projektJAVA/src/langtonsant/Ant.java
@@ -7,12 +7,69 @@ public class Ant {
     public Ant(Point startingPosition)
     {
         this.position = startingPosition;
+        this.headingDirection = Direction.South;
     }
 
-    Point position;
+    private Point position;
+    private Direction headingDirection;
+    private int howOld = 0;
 
-    public void makeStep()
-    {
+    private enum  Direction {
+        North,South,West,East;
+    }
 
+    public void makeStep(char instruction) {
+        switch (this.headingDirection) {
+            case North:
+                if (instruction == 'L') {
+                    headingDirection = Direction.West;
+                    position.x -= 1;
+                } else if (instruction == 'R') {
+                    headingDirection = Direction.East;
+                    position.x += 1;
+                }
+                break;
+            case East:
+                if (instruction == 'L') {
+                    headingDirection = Direction.North;
+                    position.y += 1;
+                } else if (instruction == 'R') {
+                    headingDirection = Direction.South;
+                    position.y -= 1;
+                }
+                break;
+            case West:
+                if (instruction == 'L') {
+                    headingDirection = Direction.South;
+                    position.y -= 1;
+                } else if (instruction == 'R') {
+                    headingDirection = Direction.North;
+                    position.y += 1;
+                }
+                break;
+            case South:
+                if (instruction == 'L') {
+                    headingDirection = Direction.East;
+                    position.x += 1;
+                } else if (instruction == 'R') {
+                    headingDirection = Direction.West;
+                    position.x -= 1;
+                }
+                break;
+        }
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public int getHowOld() {
+        return howOld;
+    }
+
+    public void increaseHowOld() {
+        howOld++;
     }
 }
+
+
